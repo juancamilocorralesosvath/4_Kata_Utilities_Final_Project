5f44f92ef20d72c8c49a4684014d398f6f7ca7fc
juanca <juancamilocorra@gmail.com>
2021-01-24 18:04:03 -0500

    added Bootstrap, imported the stylesheet (i forgot to do so :v)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import style from './styles/style.scss'
 const AMOUNT = 10;
 const category = document.getElementById("1");
 const difficulty = document.getElementById("2");
 const type = document.getElementById("3");
 const btn = document.getElementById("awesome-btn");
  // bueno, ya hicimos que una peticion funcionara
+ // aqui hacemos una peticion como un usuario la haria: con categoria,, 
+ // dificultad y tipo de respuesta 
 const myFunction = () => {
     axios.get(`https://opentdb.com/api.php?amount=${AMOUNT}&category=${Number(category.value)}&difficulty=${difficulty.value}&type=${type.value}`)
     .then((resp)=> {
